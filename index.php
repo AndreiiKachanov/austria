2f7d38a5702f9f770e751ff7f7678b87301f5f5e
Andy <andreii.kachanov@aily.team>
2024-04-02 01:07:19 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<title>Моя австрия</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="css/style_slider.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+</head>
+<body>
+	<div id="container" class="vcard">
+		<header>
+			<h1 class="logotip">
+				<a class="fn org" href="index.php">Moja Austria</a>								
+			</h1>
+			<form class="form-wrapper" action="#" method="POST">
+				<input type="text" id="search" placeholder="Поиск по сайту" required>
+				<input type="submit" value="" id="submit">				
+			</form>
+		</header>
+		<div id="slider_block">
+			<div id="sld">
+		            <div  class="opacity_border_top">
+		            	<div class="opacity"></div>
+		            	<span class="slide_desctiption">Сайт для тех, кто планирует жить в Австрии, или хочет поближе познакомиться с этой страной</span>		            	
+		            </div>
+		            <div id="slider" class="nivoSlider">
+		                <img src="images/slides/slide.jpg" alt="" />
+		                <img src="images/slides/slide2.jpg" alt="" />
+		                <img src="images/slides/slide3.jpg" alt="" />            
+		            </div>
+			</div>
+			<div id="info">
+				<p class="phone">Телефон</p>
+				<p class="tel">+356 (0)6 601 530</p>
+				<div class="workhours">
+					<p>Время работы:</p>				
+					<p>пн-пт с 9.00 до 20.00</p>
+					<p><span>сб-вс</span> с 9.00 до 18.00</p>
+				</div>
+			<button>
+				<span class="contact_us">Связаться с нами</span>
+				<span class="circle"></span>
+			</button>					
+			</div>
+			<div class="clear"></div>						
+		</div>
+		<div id="top_menu">
+			<nav>
+				<a href="#">Главная</a>
+				<a href="#">О компаниии</a>
+				<a href="#">Услуги</a>
+				<a href="#">Цены</a>
+				<a href="#">Вакансии</a>
+				<a href="#">Статьи</a>
+				<a href="#">Контакты</a>
+			</nav>
+			<div class="clear"></div>			
+		</div>
+		<div id="content_site">
+			<div id="sidebar">
+				<ul class="left_menu">
+					<li>
+						<a href="">
+							<img src="images/left_menu/1.png" alt="">
+							<span class="text">Путь иммиграции в Австрию </span>	
+						</a>						
+					</li>
+					<li>
+						<a href="">
+							<img src="images/left_menu/2.png" alt="">
+							<span class="text">Покупка и аренда недвижимости</span>	
+						</a>						
+					</li>
+					<li>
+						<a href="">
+							<img src="images/left_menu/3.png" alt="">
+							<span class="text">Полезная информация</span>	
+						</a>						
+					</li>
+					<li>
+						<a href="">
+							<img src="images/left_menu/4.png" alt="">
+							<span class="text">Об Австрии</span>	
+						</a>						
+					</li>
+					<li>
+						<a href="">
+							<img src="images/left_menu/5.png" alt="">
+							<span class="text">Изучаем словенский язык</span>	
+						</a>						
+					</li>
+					<li>
+						<a href="">
+							<img src="images/left_menu/6.png" alt="">
+							<span class="text">Как добраться в Австрию, визы</span>	
+						</a>						
+					</li>													
+				</ul>
+				<div class="feedback">
+					<h1>Обратная связь</h1>
+					<form action="#" method="POST">
+						<label for="check1">ФИО:</label>
+						<input type="text" id="check1">
+						<label for="check2">Email или Телефон:</label>
+						<input type="text" id="check2">
+						<label for="check3">Текст сообщения:</label>
+						<textarea maxlength="255" id="check3"></textarea>
+						<button type="submit">
+							<span class="contact_us">Отправить</span>
+							<span class="circle"></span>
+						</button>
+					</form>
+				</div>
+			</div>
+			<div id="main">
+				<ul id="about_austria">
+					<li class="about_li">
+						<div class="article">
+							<img src="images/article1.jpg" alt="">
+							<h1>Почему Австрия?</h1>
+							<p>В мире очень мало стран, которые могли бы предложить Вам такое разнообразие ощущений, которое дарит своим гостям Австрия.</p>
+							<h2>Основные преимущества Австрии</h2>
+							<ul>
+								<li><span>Расположение</span></li>
+								<li><span>Безопасность</span></li>
+								<li>По сравнению с Латвией и Чехией <span>легко иммигрировать</span> – за <span>325 евро в месяц</span> страховка на всю семью и право жить тут</li>
+							</ul>
+						</div>
+
+						<div class="article">
+							<img src="images/article2.jpg" alt="">
+							<h1>Преимущества работы с нами</h1>
+							<h2>Главные достоинства нашей компани</h2>
+							<ul>
+								<li><span>Оказываем консультации</span> по вопросам иммиграции, налогообложения, ведения бухгалтерии, получения социальных льгот, мед. обслуживания и т.д.</li>
+								<li>Подскажем оптимальный способ <span>выполнения требований к документам</span> гос органов</li>
+								<li>Помощь в <span>оформлении краткосрочных виз</span>, поиске/брони отеля или апартаментов, аренде автомобиля</li>
+								<li>Все Ваши накладные расходы мы стремимся уменьшить, работая с <span>проверенными бухгалтерами, нотариусом, судебными переводчиками</span></li>
+							</ul>
+						</div>
+						<div class="under_article clear">
+							<div class="left">
+								<button>
+									<span class="contact_us">Читать далее</span>
+									<span class="circle"></span>
+								</button>
+							</div>
+							<div class="right">
+								<button>
+									<span class="contact_us">Читать далее</span>
+									<span class="circle"></span>
+								</button>
+							</div>
+							<div class="clear"></div>
+						</div>
+					</li>
+				</ul>
+				<div id="news">
+					<h1>Новости</h1>
+					<div class="news_wrap">
+						<div class="date">12.08.2012</div>
+						<img src="/images/news1.jpg" alt="">
+						<h2>Покорение Альп - слет любителей скалолазания</h2>
+						<p>Прошел ежегодный слет любителей скалолазания</p>
+						<a href="">Подробнее</a>
+					</div>
+					<div class="news_wrap">
+						<div class="date">12.08.2012</div>
+						<img src="/images/news2.jpg" alt="">
+						<h2>Покорение Альп</h2>
+						<p>В середине июля 2012 года прошел ежегодный слет любителей туризма и скалолазания</p>
+						<a href="">Подробнее</a>
+					</div>
+					<div class="news_wrap">
+						<div class="date">12.08.2012</div>
+						<img src="/images/news3.jpg" alt="">
+						<h2>Покорение Альп</h2>
+						<p>Прошел ежегодный слет любителей скалолазания</p>
+						<a href="">Подробнее</a>
+					</div>
+					<div class="news_wrap">
+						<div class="date">12.08.2012</div>
+						<img src="/images/news4.jpg" alt="">
+						<h2>Покорение Альп</h2>
+						<p>Прошел ежегодный слет любителей скалолазания</p>
+						<a href="">Подробнее</a>
+					</div>
+					<div class="news_wrap">
+						<div class="date">12.08.2012</div>
+						<img src="/images/news5.jpg" alt="">
+						<h2>Покорение Альп</h2>
+						<p>Прошел ежегодный слет любителей скалолазания</p>
+						<a href="">Подробнее</a>
+					</div>
+					<a class="clear subscribe" href="#">
+						<span>Подписаться на расылку новостей и анонсов</span>
+						<div class="circle"></div>
+						<div class="clear"></div>
+					</a>															
+					<div class="clear"></div>	
+				</div>
+			</div>
+			<div class="clear"></div>
+			<footer>
+				<div class="copyright"><a href="http://www.site.ru">&#169; 2012 site.ru</a></div>
+				<div id="bottom_menu">
+					<a href="#">Главная</a>
+					<a href="#">О компаниии</a>
+					<a href="#">Услуги</a>
+					<a href="#">Цены</a>
+					<a href="#">Контакты</a>
+					<div class="clear"></div>
+				</div>
+				<div class="contacts">
+					<p class="tel">+386 (0)1 6001 530</p>
+					<a class="mail" href="mailto:info@site.ru">info@site.ru</a>
+				</div>					
+			</footer>	
+		</div>
+	</div>
+	<script type="text/javascript" src="js/jquery-1.4.3.min.js"></script>
+	<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+	<script type="text/javascript">
+    	$(window).load(function() {
+        $('#slider').nivoSlider({directionNavHide:false});
+    });
+    </script>
+</body>
+</html>
